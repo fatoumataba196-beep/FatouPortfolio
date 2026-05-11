d2f78c0dfdf4cf047f1d0ba446b4d06aed5a90a0
fatoumataba196-beep <fatoumataba196@gmail.com>
2026-05-11 18:06:55 +0000

    Add files via upload

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+- [x] Ajouter le bouton "CV (PDF)" dans la section Accueil (dans le même conteneur `.hero-buttons` à côté de "Me contacter").
+- [ ] Vérifier le rendu sur desktop et mobile.
+
+
---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1q5_auZMOQpU7ajI6XFFFGU1gd4OQ3BI",
+  authDomain: "fatouportfolio.firebaseapp.com",
+  projectId: "fatouportfolio",
+  storageBucket: "fatouportfolio.firebasestorage.app",
+  messagingSenderId: "184204186828",
+  appId: "1:184204186828:web:99e26791810320b03ede69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
